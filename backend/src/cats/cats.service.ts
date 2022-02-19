91e5e4efd53853e1c4fd4ab34db0e3f5267d16fe
Nick Ducker <nick_ducker@live.com>
2022-02-19 19:28:44 +1100

    add client for cat API

--- backend/src/cats/cats.service.ts
+++ backend/src/cats/cats.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+  private _client: AxiosInstance;
+
+  private client(): AxiosInstance {
+    if (!this._client) {
+      this._client = axios.create({
+        baseURL: this.apiBase,
+        headers: this.apiHeaders,
+      });
+    }
+
+  return this._client;
+  }
+
+  get apiBase(): string {
+    return process.env.CAT_API_ENDPOINT;
+  }
+
+  get apiHeaders(): Record<string, string> {
+    return {
+      'x-api-key': `${process.env.CAT_API_KEY}`,
+    };
+  }
+}
